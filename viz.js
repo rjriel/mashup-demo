cbb796acad8161194ac7b90a05a93511d6023855
Rene-Jacques Riel <rie@qlik.com>
2017-03-08 11:18:01 -0500

    adding getting started, adding code separator

--- viz.js
+++ viz.js
@@ -3,58 +3,57 @@ var config = {
     port: 80,
     prefix: "/",
     isSecure: false
-};
+}
 require.config({
     baseUrl: (config.isSecure ? "https://" : "http://") + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources"
-});
+})
 
 require(["js/qlik"], function (qlik) {
     qlik.setOnError(function (error) {
         console.log(error)
-        $("#error span").html(error.message);
-        $("#error").show();
+        $("#error span").html(error.message)
+        $("#error").show()
         $("#closeerror").on("qv-activate", function () {
-            $("#error").hide();
-        });
-    });
+            $("#error").hide()
+        })
+    })
 
-    var app = qlik.openApp("381a4a76-9f48-47fc-94a4-78743f642581", config);
+    var app = qlik.openApp("381a4a76-9f48-47fc-94a4-78743f642581", config)
 
-    var barCreated = false;
+    var barCreated = false
     window.createBar = function () {
         if (!barCreated) {
-            barCreated = true;
+            barCreated = true
             app.visualization.create('barchart', [
                 "Shape",
                 { qDef: { qDef: "=Count([PostedDate])", qLabel: "Sighting Count" } }])
                 .then(function (vis) {
-                    vis.show("vizBasicDiv");
-
-                });
+                    vis.show("vizBasicDiv")
+                })
         }
     }
 
-    var extCreated = false;
+    var extCreated = false
     window.createExt = function () {
         if (!extCreated) {
-            extCreated = true;
+            extCreated = true
             app.visualization.create('senseui-barchart', [
                 "State",
                 { qDef: { qDef: "=Count(State)", qLabel: "Sighting Count" } }])
                 .then(function (vis) {
-                    vis.show("vizExtDiv");
-                });
+                    vis.show("vizExtDiv")
+                })
         }
     }
 
-    var getCreated = false;
+    var getCreated = false
     window.createGet = function () {
         if (!getCreated) {
-            getCreated = true;
+            getCreated = true
             app.visualization.get('CCapKa')
                 .then(function (vis) {
-                    vis.show("vizGetDiv");
-                });
+                    vis.show("vizGetDiv")
+                })
         }
     }
 })
